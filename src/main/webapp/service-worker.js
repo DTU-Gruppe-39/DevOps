3dea87a920225f502480b7120c064c08498c7df6
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-09-10 12:09:12 +0200

    test

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3447e9d256779b06e44745a7e491198.js"
+  "/precache-manifest.0ab667bf01227dfa4c4765d344b15ef7.js"
 );
 
 self.addEventListener('message', (event) => {
--- web/src/App.js
+++ web/src/App.js
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
         <p>Windows 120Hz</p>
+          <p>
+              Magnus er en luder!
+          </p>
       </header>
     </div>
   );
