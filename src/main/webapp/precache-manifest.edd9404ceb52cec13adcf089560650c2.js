9af7d72d881b25ab778df97dad7101e78308a03f
Eksimo <soren@jasonic.dk>
2019-09-17 10:37:03 +0200

    hero push

---
+++ src/main/webapp/precache-manifest.edd9404ceb52cec13adcf089560650c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7799b9f80677ded94690ce539a581e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5c98a94793e2955d543",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "634fdf86a8b3b46db875",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b5c98a94793e2955d543",
+    "url": "/static/js/2.f3554b85.chunk.js"
+  },
+  {
+    "revision": "634fdf86a8b3b46db875",
+    "url": "/static/js/main.1fda1153.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34ac8a9f42df27208b5708ff802b963b.js"
+  "/precache-manifest.edd9404ceb52cec13adcf089560650c2.js"
 );
 
 self.addEventListener('message', (event) => {
