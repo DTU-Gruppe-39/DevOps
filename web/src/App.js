4c0a40c0fd565badd27985923e2277ba5f8e91d9
rallerG <raller799@live.dk>
2019-10-01 11:34:54 +0200

    cleaned project

--- web/src/App.js
+++ web/src/App.js
@@ -6,52 +6,14 @@ import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 import {BrowserRouter as Router, Route, Link, withRouter, Switch} from "react-router-dom";
-import GiraffeStore from "./stores/GiraffeStore";
 import {observer} from "mobx-react";
 
-const giraffeStore = new GiraffeStore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="d-flex justify-content-between">
-        <InputGroup className="mb-3">
-          <FormControl
-              placeholder="Recipient's username"
-              aria-label="Recipient's username"
-              aria-describedby="basic-addon2"
-          />
-        </InputGroup>
-        <Button className="mb-3" variant="primary">Primary</Button>
-        </div>
-        <p>Windows 120Hz</p>
-        <Switch>
-          <Route path={"/about/:text"} component={About}/>
-          <Route exact path ={"/"} render={ () => <h1>Startside Stress</h1> } />
-          <Route render={()=><h1>404</h1>} />
-        </Switch>
 
-          <p>
-              Nedenstående er giraffer! wgwg
-          </p>
-        <ul>
-          {giraffeStore.giraffes.map((giraffeName, key) =>
-          <li key={key}>{giraffeName}</li>)}
-        </ul>
-        <Button variant="primary" onClick={()=>giraffeStore.giraffes.push("Rasmus")}>Tilføj giraf</Button>
       </header>
     </div>
   );
--- web/src/App.test.js
+++ web/src/App.test.js
@@ -10,22 +10,3 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-test('Silly test',()=>{
-  expect(2+2).toBe(4);
-});
-
-test('loads items eventually', async () => {
-  const { getByText } = render(<HashRouter><App /></HashRouter>);
-
-  // Click button
-  fireEvent.click(getByText('Tilføj giraf'));
-
-  // Wait for page to update with query text
-  const items = await getByText("Rasmus");
-  expect(items).toBeInTheDocument();
-});
-
-it('renders welcome message', () => {
-  const { getByText } = render(<HashRouter><App /></HashRouter>);
-  expect(getByText('Learn React')).toBeInTheDocument();
-});
