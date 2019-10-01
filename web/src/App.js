f26b8c53331f847218b1fd5e3a5bfd7042585a96
Cilian <soren@jasonic.dk>
2019-10-01 12:49:47 +0200

    sidebar

--- web/src/App.js
+++ web/src/App.js
@@ -48,21 +48,24 @@ function App() {
           </nav>
         </div>
 
-        <header className="App-header">
 
+        <div className="d-flex" id="wrapper">
+        <div className="sidebar">
           <div className="bg-light border-right" id="sidebar-wrapper">
-            <div className="sidebar-heading">Start Bootstrap</div>
+            <div className="sidebar-heading"></div>
             <div className="list-group list-group-flush">
-              <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Shortcuts</a>
+              <a href="#" className="list-group-item list-group-item-action bg-light">Use cases</a>
+              <a href="#" className="list-group-item list-group-item-action bg-light">Stakeholders</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
             </div>
           </div>
-
-        </header>
+        </div>
+        </div>
+        {/*<header className="App-header">*/}
+        {/*</header>*/}
       </div>
   );
 }
