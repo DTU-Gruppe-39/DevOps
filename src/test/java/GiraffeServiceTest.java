0017ba5dab1878fcadccc895f2b4fac83a11815e
rallerG <raller799@live.dk>
2019-10-01 10:34:33 +0200

    vdrrw

---
+++ src/test/java/GiraffeServiceTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+public class GiraffeServiceTest {
+
+    @org.junit.Test
+    public void getGiraffes() {
+    }
+}
--- web/src/App.test.js
+++ web/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {HashRouter} from "react-router-dom";
+import { render } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,3 +13,8 @@ it('renders without crashing', () => {
 test('Silly test',()=>{
   expect(2+2).toBe(4);
 })
+
+it('renders welcome message', () => {
+  const { getByText } = render(<HashRouter><App /></HashRouter>);
+  expect(getByText('Learn React')).toBeInTheDocument();
+});
