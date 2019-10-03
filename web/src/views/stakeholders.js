d77e05263212f872812f61822eb5c41a05f0aa13
rallerG <raller799@live.dk>
2019-10-03 13:22:59 +0200

    stakeholder view

--- web/src/views/stakeholders.js
+++ web/src/views/stakeholders.js
@@ -1,10 +1,42 @@
 import React from "react";
 
 class Stakeholders extends React.Component {
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="stakeholder-form">
-                
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Contact person:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <div className="radio">
+                        <label>
+                            <input type="radio" value="Direct" checked={true} />
+                            Direct
+                        </label>
+                    </div>
+                    <div className="radio">
+                        <label>
+                            <input type="radio" value="Indirect" />
+                            Indirect
+                        </label>
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         );
     }
