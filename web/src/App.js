3d6e2a8823950052a964165c24dca1cb3f07888a
rallerG <raller799@live.dk>
2019-10-03 12:56:59 +0200

    small fix to layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Ionic_Logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -11,38 +11,20 @@ import './App.css';
 function App() {
   return (
       <div className="App">
+          <div className="topbar">
+              <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                  <img src={logo} />
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
 
-          <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-            <button className="btn btn-outline-light" id="menu-toggle">Toggle Menu</button>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </nav>
+                </div>
+              </nav>
+          </div>
         {/*</div>*/}
 
 
@@ -50,18 +32,19 @@ function App() {
           <div className="bg-light border-right">
             <div className="sidebar-heading space"></div>
             <div className="list-group list-group-flush">
+              <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
+              <a href="#/stakeholders" className="list-group-item list-group-item-action bg-light">Stakeholders</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Use cases</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Stakeholders</a>
-              <a href="#" className="list-group-item list-group-item-action bg-light">Overview</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Events</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Profile</a>
               <a href="#" className="list-group-item list-group-item-action bg-light">Status</a>
             </div>
         </div>
         </div>
-
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="main">
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
         {/*<header className="App-header">*/}
         {/*</header>*/}
       </div>
---
+++ web/src/views/stakeholders.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Stakeholders extends React.Component {
+    render() {
+        return (
+            <div className="stakeholder-form">
+                
+            </div>
+        );
+    }
+}
+export default Stakeholders;
