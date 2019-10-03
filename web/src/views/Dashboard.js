4c58b169fe19f9fd425761c9872d4f67c69acc0d
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-10-03 14:32:14 +0200

    Routing and created corresponding classes

---
+++ web/src/views/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Dashboard extends React.Component{
+    render() {
+        return(
+            <p>This is a Dashboard!</p>
+        );
+    }
+}
+export default Dashboard;
---
+++ web/src/views/Events.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Events extends React.Component{
+    render() {
+        return(
+            <p>This is an event list</p>
+        );
+    }
+}
+export default Events;
