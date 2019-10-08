ff34792ea5a6733fbad491034365ea49405a4fbe
MagnusKochDTU <s175189@student.dtu.dk>
2019-10-08 10:23:10 +0200

    Update MongoConnector.java
    fixes bugs

--- src/main/java/MongoConnector.java
+++ src/main/java/MongoConnector.java
@@ -1,19 +1,32 @@
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MongoConnector {
-    private String username = System.getenv("magnus");
-    private String password = System.getenv("kqly123");
-    private String dbName = "test";
+    private static String username = System.getenv("devopsusername");
+    private static String password = System.getenv("devopspassword");
 
     public MongoDatabase getDb() {
         MongoClient mongoClient = MongoClients.create("mongodb+srv://"+ username +":" +password + "@devops69-1bknv.mongodb.net/admin?retryWrites=true&w=majority");
-        return mongoClient.getDatabase(dbName);
+        MongoDatabase database = mongoClient.getDatabase("devops");
+        return database;
     }
 
+    //testing
     public static void main(String[] args) {
         MongoDatabase test = new MongoConnector().getDb();
         System.out.println("Connected to " + test.getName());
+
+        Document doc = new Document("name","Brian");
+        MongoCollection<Document> brugere = test.getCollection("brugere");
+        brugere.insertOne(doc);
+        List<Document> docs = new ArrayList<>();
+        brugere.find().into(docs);
+        System.out.println(docs);
     }
 }
