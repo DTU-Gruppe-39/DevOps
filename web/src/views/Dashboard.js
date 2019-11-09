78ca6be986ec01752a65cab868e4814589b2b05f
rallerG <raller799@live.dk>
2019-11-09 14:01:43 +0100

    Progessbar needs CSS

--- web/src/views/Dashboard.js
+++ web/src/views/Dashboard.js
@@ -1,10 +1,37 @@
 import React from "react";
+import {observer} from "mobx-react";
+import {ProgressBar} from "react-bootstrap";
+import {progressStore} from "../stores/ProgressStore";
+import {visionStore} from "../stores/VisionStore";
 
-class Dashboard extends React.Component{
-    render() {
+    function Dashboard() {
         return(
-            <p>This is a Dashboard!</p>
+            <div className="dashboard-progress">
+                <ProgressBar now={progressStore.progress} label={`${progressStore.progress}%`} />
+                <div className="progress-input">
+                    <form className="progressinputform" onSubmit={getOnSubmit()}>
+                        <label>
+                                <input name="progress"
+                                          type="number" placeholder="Update progress"
+                                          onFocus={() => (progressStore.progress !== 0) ? progressStore.inputProgress = progressStore.progress : null}
+                                          value={progressStore.inputProgress}
+                                          onChange={(e) => progressStore.inputProgress = e.target.value} required/>
+                        </label>
+                        <input type="submit" value="Save"/>
+                    </form>
+
+                </div>
+            </div>
         );
     }
-}
-export default Dashboard;
+
+    function getOnSubmit() {
+        return (e) => {
+            e.preventDefault();
+            progressStore.progress = progressStore.inputProgress;
+            progressStore.inputProgress = "";
+
+        }
+    }
+
+export default observer(Dashboard);
