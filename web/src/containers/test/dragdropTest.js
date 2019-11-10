8c319fde8646328cf93bccae78445c41d6d3ec40
Cilian <32563603+Cilian@users.noreply.github.com>
2019-11-10 20:09:27 +0100

    CSS

--- web/src/containers/test/dragdropTest.js
+++ web/src/containers/test/dragdropTest.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Draggable from "../draggable";
 import Droppable from "../droppable";
 import {taskStore} from "../../stores/TaskStore";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Wrapper = styled.div`
     // width: 100%;
@@ -23,36 +25,52 @@ const droppableStyle = {
     backgroundColor: '#555',
     width: '350px',
     height: '550px',
-    margin: '36px',
+    margin: '30px',
     padding: '8px'
 };
 
 function checkTaskStatus() {
-        taskStore.taskList.map(function (task, key) {
-            console.log(task.taskStatus.toString());
-            if (task.taskStatus.toString() === "In progress") {
-                    taskStore.taskList.map((task, key) => (
-                            console.log(task.taskName.toString()),
-                                <Item><p key={key}>{task.taskName}</p></Item>
-                        ),
-                    )
-            } else {
-            }
-        })
+    taskStore.taskList.map(function (task, key) {
+        console.log(task.taskStatus.toString());
+        if (task.taskStatus.toString() === "In progress") {
+            taskStore.taskList.map((task, key) => (
+                    console.log(task.taskName.toString()),
+                        <Item><p key={key}>{task.taskName}</p></Item>
+                ),
+            )
+        } else {
+        }
+    })
 }
 
-    export default class containers extends React.Component{
-        render() {
-            return (
+export default class containers extends React.Component{
+    render() {
+        return (
+            <Row className="justify-content-center">
+
                 <Wrapper>
-                    <Droppable id="drop1" style={droppableStyle}>
-                        <Draggable id="item1">
-                            {checkTaskStatus()}
-                        </Draggable>
-                        <Draggable id="item2"><Item>Dette er en case</Item></Draggable>
-                    </Droppable>
-                    <Droppable id="drop2" style={droppableStyle}/>
+                    <Col md="auto">
+                        <h3><center>To-Do</center></h3>
+                        <Droppable id="drop1" style={droppableStyle}>
+                            <Draggable id="item1">
+                                {checkTaskStatus()}
+                            </Draggable>
+                            <Draggable id="item2"><Item>Dette er en case</Item></Draggable>
+                        </Droppable>
+                    </Col>
+
+                    <Col md="auto">
+                        <h3><center>In-Progress</center></h3>
+                        <Droppable id="drop2" style={droppableStyle}/>
+                    </Col>
+
+                    <Col md="auto">
+                        <h3><center>Completed</center></h3>
+                        <Droppable id="drop3" style={droppableStyle}/>
+                    </Col>
                 </Wrapper>
-            )
-        }
+
+            </Row>
+        )
     }
+}
