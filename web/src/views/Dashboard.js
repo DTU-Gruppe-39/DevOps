2ece04e5c5d8425c7179e843f9d773a659f8ff67
rallerG <raller799@live.dk>
2019-11-10 13:44:31 +0100

    CSS added to dashboard

--- web/src/views/Dashboard.js
+++ web/src/views/Dashboard.js
@@ -2,23 +2,33 @@ import React from "react";
 import {observer} from "mobx-react";
 import {ProgressBar} from "react-bootstrap";
 import {progressStore} from "../stores/ProgressStore";
-import {visionStore} from "../stores/VisionStore";
-
+import "./Dashboard.css"
     function Dashboard() {
         return(
             <div className="dashboard-progress">
-                <ProgressBar now={progressStore.progress} label={`${progressStore.progress}%`} />
-                <div className="progress-input">
-                    <form className="progressinputform" onSubmit={getOnSubmit()}>
-                        <label>
-                                <input name="progress"
-                                          type="number" placeholder="Update progress"
-                                          onFocus={() => (progressStore.progress !== 0) ? progressStore.inputProgress = progressStore.progress : null}
-                                          value={progressStore.inputProgress}
-                                          onChange={(e) => progressStore.inputProgress = e.target.value} required/>
-                        </label>
-                        <input type="submit" value="Save"/>
-                    </form>
+                <div class="container">
+                    {/*<div className="w-100"></div>*/}
+                    <div class="row justify-content-center">
+                        <div class="col-10">
+                            <ProgressBar now={progressStore.progress} label={`${progressStore.progress}%`} style={{height: 40}} />
+                        </div>
+                    </div>
+                    <div className="progress-input">
+                        <div className="row justify-content-center">
+                            <div className="col-sm-4">
+                                <form className="progressinputform" onSubmit={getOnSubmit()}>
+                                    <label>
+                                            <input name="progress"
+                                                   type="number" placeholder="Update progress"
+                                                   onFocus={() => (progressStore.progress !== 0) ? progressStore.inputProgress = progressStore.progress : null}
+                                                   value={progressStore.inputProgress}
+                                                   onChange={(e) => progressStore.inputProgress = e.target.value} required/>
+                                    </label>
+                                    <input type="submit" value="Save"/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
