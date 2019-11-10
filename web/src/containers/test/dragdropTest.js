5f10cb022cb4475b6099ec1c44ca52e2f6e4f173
Cilian <soren@jasonic.dk>
2019-11-10 19:55:19 +0100

    task into kanban

--- web/src/containers/test/dragdropTest.js
+++ web/src/containers/test/dragdropTest.js
@@ -2,38 +2,57 @@ import React from "react";
 import styled from "styled-components";
 import Draggable from "../draggable";
 import Droppable from "../droppable";
+import {taskStore} from "../../stores/TaskStore";
 
 const Wrapper = styled.div`
-    width: 100%;
+    // width: 100%;
     padding 32px;
     display: flex;
     justify-content: center;
     `;
 
 const Item = styled.div`
-    padding: 8px;
+    padding: 16px;
     color: #555;
+    margin: 8px;
     background-color: white;
     border-radius: 3px;
 `;
 
 const droppableStyle = {
     backgroundColor: '#555',
-    width: '200px',
-    height: '400px',
-    margin: '32px'
+    width: '350px',
+    height: '550px',
+    margin: '36px',
+    padding: '8px'
 };
 
-export default class containers extends React.Component{
-    render() {
-        return (
-            <Wrapper>
-                <Droppable id="drop1" style={droppableStyle}>
-                    <Draggable id="item1"> <Item> Tester</Item></Draggable>
-                    <Draggable id="item2"><Item>Dette er en case</Item></Draggable>
-                </Droppable>
-                <Droppable id="drop2" style={droppableStyle}/>
-            </Wrapper>
-        )
-    }
+function checkTaskStatus() {
+        taskStore.taskList.map(function (task, key) {
+            console.log(task.taskStatus.toString());
+            if (task.taskStatus.toString() === "In progress") {
+                    taskStore.taskList.map((task, key) => (
+                            console.log(task.taskName.toString()),
+                                <Item><p key={key}>{task.taskName}</p></Item>
+                        ),
+                    )
+            } else {
+            }
+        })
 }
+
+    export default class containers extends React.Component{
+        render() {
+            return (
+                <Wrapper>
+                    <Droppable id="drop1" style={droppableStyle}>
+                        <Draggable id="item1">
+                            {checkTaskStatus()}
+                        </Draggable>
+                        <Draggable id="item2"><Item>Dette er en case</Item></Draggable>
+                    </Droppable>
+                    <Droppable id="drop2" style={droppableStyle}/>
+                </Wrapper>
+            )
+        }
+    }
