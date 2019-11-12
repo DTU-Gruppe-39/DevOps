25345735bf80f54d07d659876106f2067b0c0707
rallerG <raller799@live.dk>
2019-11-12 11:21:20 +0100

    package

---
+++ src/main/webapp/precache-manifest.c64a351733261f20668346b5172d0fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bbfadad07e56c2d3422b8bbce0f0c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "327e9902216591dfd67a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "99418c5ba6528d7c58c1",
+    "url": "/static/css/main.9716c132.chunk.css"
+  },
+  {
+    "revision": "327e9902216591dfd67a",
+    "url": "/static/js/2.cb938aee.chunk.js"
+  },
+  {
+    "revision": "99418c5ba6528d7c58c1",
+    "url": "/static/js/main.bd1df4db.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
