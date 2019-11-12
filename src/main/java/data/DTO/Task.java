4cbba7cc42cc420a8e74a502e327dcbb72c8773c
MagnusKochDTU <s175189@student.dtu.dk>
2019-11-12 09:17:06 +0100

    Updated Task classes

--- src/main/java/data/DTO/Task.java
+++ src/main/java/data/DTO/Task.java
@@ -6,7 +6,11 @@ import java.util.Map;
  * Created by magnus
  */
 public class Task extends DocumentObject {
+  private String id;
   private String name;
+  private String description;
+  private User responsible;
+  private Status status;
 
   public Task(String name) {
     this.name = name;
