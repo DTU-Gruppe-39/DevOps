ea3fa065180f7c3c133f6233d1c4a920ddfc8578
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-11-26 12:01:23 +0100

    fix

---
+++ src/main/webapp/precache-manifest.10550e43d380d85b3c6a297c596b0448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a011317381444f197d548f51a6dd25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "445059c29f51e8da6725",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "dc765f28c8425a55ec43",
+    "url": "/static/css/main.3e495474.chunk.css"
+  },
+  {
+    "revision": "445059c29f51e8da6725",
+    "url": "/static/js/2.107c085d.chunk.js"
+  },
+  {
+    "revision": "dc765f28c8425a55ec43",
+    "url": "/static/js/main.d6c0efb4.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
