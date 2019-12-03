80934f46402ff1e6c680c2e225583bb127bfabfd
rallerG <raller799@live.dk>
2019-12-03 12:03:20 +0100

    package

---
+++ src/main/webapp/precache-manifest.432fa103a4d4a1edb78e30095bf34ebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf37b96353f345e4f6552b0d2e603e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23902c06ddac427b4012",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f37694f2af5cc720950f",
+    "url": "/static/css/main.65053128.chunk.css"
+  },
+  {
+    "revision": "23902c06ddac427b4012",
+    "url": "/static/js/2.d17895f3.chunk.js"
+  },
+  {
+    "revision": "f37694f2af5cc720950f",
+    "url": "/static/js/main.b0039394.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "6c613680b8906e10ec36022dc710d198",
+    "url": "/static/media/spotify-2-logo-png-transparent.6c613680.png"
+  }
+]);
--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f2cbb4b5c4687021c60e7966e916d83.js"
+  "/precache-manifest.432fa103a4d4a1edb78e30095bf34ebd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
